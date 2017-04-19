a9fe26652f6d9946e66cd40721f8ea788793d535
Sarah Dueweke <sdueweke@gmail.com>
2017-04-19 18:25:25 +1200

    Set up random rgba and x/y axis for bouncing rectangles

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,20 @@
-/* set up basic canvas body */
+/* set up variables for basic canvas body and context */
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+
+/* create random color variables */
+var r = (Math.floor(Math.random() * 256));
+var g = (Math.floor(Math.random() * 256));
+var b = (Math.floor(Math.random() * 256));
+var a = 1;
+
+/* create random starting axis for blocks */
+var x = (Math.random()* canvas.width);
+var y = (Math.random()* canvas.height);
+
+/* drop random rectangle "blocks" */
+c.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+c.fillRect(x, y, 100, 100);
