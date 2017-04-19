833c447246007504f3f3fc7b3b7d7c45c0a9bb79
Sarah Dueweke <sdueweke@gmail.com>
2017-04-19 19:24:04 +1200

    Set up function for bounce animation

--- JS/script.js
+++ JS/script.js
@@ -11,10 +11,28 @@ var g = (Math.floor(Math.random() * 256));
 var b = (Math.floor(Math.random() * 256));
 var a = 1;
 
-/* create random starting axis for blocks */
+/* create random starting axis for blocks and ending points for animation*/
 var x = (Math.random()* canvas.width);
-var y = (Math.random()* canvas.height);
+var y = 0;
+var mx;
+var my;
 
 /* drop random rectangle "blocks" */
-c.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
-c.fillRect(x, y, 100, 100);
+
+function blockDrop() {
+  y++;
+
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  c.beginPath();
+  c.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+  c.closePath();
+  c.fillRect(x, y, 100, 100);
+
+
+  requestAnimFrame(blockDrop);
+
+};
+
+/* Begin dropping blocks! */
+blockDrop();
