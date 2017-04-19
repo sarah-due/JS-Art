0c242c6cbc0a8afa69df678d56d1517f650ea8b1
Sarah Dueweke <sdueweke@gmail.com>
2017-04-19 13:54:07 +1200

    Add js pseudocode

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/* pseudocode:
+
+onload - hide all bubbles
+randomize color and location of one bubble
+
+click function - onclick make bubble burst and play random sound
+randomize color and location of new bubble
